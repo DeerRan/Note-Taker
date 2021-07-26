84eb22f8355cafa18644b3ecad307a05fac1078e
DeerRan <77216956+DeerRan@users.noreply.github.com>
2021-07-25 23:01:35 -0500

    Figuring out why page is broken but deployed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 //Middleware
-app.use(express.static('public'));
+app.use(express.static('Develop'));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
